66b097f36b758f40569298f5d6cf3d6453b7d9d8
민구 <127157142+Mingu79@users.noreply.github.com>
2023-03-20 20:27:17 +0900

    Update Switch.java

--- Chapter1/src/Switch.java
+++ Chapter1/src/Switch.java
@@ -6,10 +6,8 @@ public class Switch {
 		// TODO Auto-generated method stub
 System.out.println("[사용자로부터 사칙연산자 +, -, *, / 값을 입력 받은 후 연산 수행 실시");
 		
-		//사용자로부터 데이터를 입력 받기 위해 Scanner 객체 선언
 		Scanner scan = new Scanner(System.in);
 		
-		//첫번째, 두번째 정수를 입력받는다
 		System.out.print("첫 번째 값 : ");
 		int one = scan.nextInt();
 		
@@ -18,7 +16,6 @@ System.out.println("[사용자로부터 사칙연산자 +, -, *, / 값을 입력
         
 		int result = 0;
 		
- 		//기호를 입력받는다
 		System.out.print("(+, -, *, /) 중 연산자를 선택해 주세요 : ");
 		String sign = scan.next();
 		
