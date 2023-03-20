d981062ad5743b6fefaaea58d98a84987540598b
HOME <HOME@LAPTOP-GEE4JE54>
2023-03-20 20:16:42 +0900

    ㄴ

--- Chapter1/src/Ifelse.java
+++ Chapter1/src/Ifelse.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
 public class Ifelse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		//첫번째, 두번째 정수를 입력받는다
+		System.out.print("첫 번째 값 : ");
+		int one = scan.nextInt();
+		
+		System.out.print("두 번째 값 : ");
+		int two = scan.nextInt();
+        
+		int result = 0;
+		
+ 		//기호를 입력받는다
+		System.out.print("(+, -, *, /) 중 연산자를 선택해 주세요 : ");
+		String sign = scan.next();
+		
+		boolean isZero=false;
+		
+	    if(sign.equals("+")) result=one+two;
+	    else if(sign.equals("-")) result=one-two;
+	    else if(sign.equals("*")) result=one*two;
+	    else if(sign.equals("/")) {
+	    	if(two==0) isZero=true;
+	    	else result=one/two;
+	    }
+	    else if(sign.equals("%")) result=one%two;
+	    
+	    if(isZero==true) System.out.println("0으로 나눌 수 없습니다.");
+	    System.out.println(one+sign+two+"의 결과는 "+result);
 	}
 
 }
--- Chapter1/src/Switch.java
+++ Chapter1/src/Switch.java
@@ -1,9 +1,50 @@
+import java.util.Scanner;
 
 public class Switch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("[사용자로부터 사칙연산자 +, -, *, / 값을 입력 받은 후 연산 수행 실시");
+		
+		//사용자로부터 데이터를 입력 받기 위해 Scanner 객체 선언
+		Scanner scan = new Scanner(System.in);
+		
+		//첫번째, 두번째 정수를 입력받는다
+		System.out.print("첫 번째 값 : ");
+		int one = scan.nextInt();
+		
+		System.out.print("두 번째 값 : ");
+		int two = scan.nextInt();
+        
+		int result = 0;
+		
+ 		//기호를 입력받는다
+		System.out.print("(+, -, *, /) 중 연산자를 선택해 주세요 : ");
+		String sign = scan.next();
+		
+		boolean isZero=false;
+	    
+	    switch (sign) {
+        case "+":  result=one+two;
+                 break;
+        case "-":  result=one+two;
+        	break;
+        case "*":  result=one*two;
+        break;
+        
+        case "/":  
+    	    	if(two==0) isZero=true;
+    	    	else result=one/two;
+        break;
+        	case "%":  
+        		result=one%two;
+            break;
+        
+        }
+	    	
+	    
+	    if(isZero==true) System.out.println("0으로 나눌 수 없습니다.");
+	    System.out.println(one+sign+two+"의 결과는 "+result);
 	}
 
 }
